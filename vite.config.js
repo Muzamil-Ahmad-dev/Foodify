6fb01da625bf9971a35f1770d55161cc0b577d03
Muzamil Ahmad <muzamilahmad.dev@gmail.com>
2025-10-15 18:05:09 +0500

    Fix vite path import for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'   // ✅ Add this line
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-
   ],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
